e50edc6f52a3e31acda45743bc3cee9731cef3c7
Enrique080 <145616603+Enrique080@users.noreply.github.com>
2023-09-22 14:18:57 -0700

    Add files via upload

---
+++ drawLine.m
@@ -0,0 +1,12 @@
+
+function lineHandle = drawLine ( pointA, pointB, color)
+  % This function two points pointA,pointB and draw a line between them
+  % with color color
+  %extract the x coordinates from the points
+  x=[pointA(1) ; pointB(1)];
+  y=[pointA(2) ; pointB(2)];
+
+  lineHandle = plot(x,y,color);
+set(lineHandle,"LineWidth",3);
+
+endfunction

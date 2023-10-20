f3abc39e99a40fbe34afe32a0891694690a880eb
Enrique080 <145616603+Enrique080@users.noreply.github.com>
2023-10-20 10:17:50 -0700

    Update crabs.m

--- crabs.m
+++ crabs.m
@@ -14,17 +14,10 @@ yCapt = 1000;
 thetaCapt = -pi/2;
 sizeCapt = 50;
 
-%initialize crab location, heading and size
-xCrab = 1000;
-yCrab = 1200;
-thetaCrab = -pi/2;
-sizeCrab = 50;
-
 % Draw the captain and initialize graphics handles
 %*********************************************************
 % Put your call to drawCapt() here ..... You must give drawCapt its
 captGraphics = drawCapt(xCapt , yCapt , thetaCapt , sizeCapt);
-crabGraphics = drawCrab(xCrab,yCrab,thetaCrab,sizeCrab);
 % input and output arguments.
 %*******************************************************
 
@@ -36,11 +29,11 @@ while(cmd != "Q")
 cmd = kbhit();
 
  if((cmd == "w") || (cmd == "a") || (cmd == "d"))
- 
+
  %erase old captGraphics
  for(i=1: length(captGraphics))
  set( captGraphics(i), 'Visible', 'off');
-  endfor
+endfor
 
  %move captain
 [xCapt, yCapt,thetaCapt] = moveCaptain(cmd, xCapt, yCapt, thetaCapt, mapWidth, mapHeight);
@@ -48,38 +41,23 @@ cmd = kbhit();
 %draw new captain
 captGraphics = drawCapt (xCapt , yCapt , thetaCapt , sizeCapt);
 
-elseif (cmd == "i" || cmd == "j" || cmd == "k" || cmd == "l" || cmd ==",") % respond crab moved
-
-%erase old crab
-  for i=1:length(crabGraphics)
-  set(crabGraphics(i),'Visible','off');
-  endfor
-
-%move crab
-[xCrab,yCrab,thetaCrabs] = moveCrab(cmd,xCrab,yCrab,thetaCrab,sizeCrab, mapHeight, mapWidth);
-
-%draw new captain and crab
-crabGraphics = drawCrab(xCrab,yCrab,thetaCrab,sizeCrab)
-
 endif
 
 if((cmd == "f") || (cmd == "g"))
 
 %erase old captGraphics
-   for(i=1: length(captGraphics))
-   set( captGraphics(i), 'Visible', 'off');
-  endfor
+ for(i=1: length(captGraphics))
+ set( captGraphics(i), 'Visible', 'off');
+endfor
 
 [sizeCapt] = getHulk(cmd, sizeCapt);
 
 captGraphics = drawCapt (xCapt , yCapt , thetaCapt , sizeCapt);
 
 endif
-
 endwhile
 
- close all
- clear
+
 
 endfunction
 
